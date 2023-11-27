3638e0219d116ceeb16e86f558c6ca0b7385f28e
Warhair <tegzesmarton@gmail.com>
2023-11-27 10:26:53 +0100

    lista_elkeszitve

--- hengerProgram/src/hengerprogram/HengerProgram.java
+++ hengerProgram/src/hengerprogram/HengerProgram.java
@@ -2,15 +2,19 @@
 package hengerprogram;
 
 import java.awt.List;
+import java.util.ArrayList;
+import java.util.Collections;
+
 
 public class HengerProgram {
 
+    private ArrayList<Henger> hengerek;
   
     public static void main(String[] args) {
-        new HengerProgram();
+        new HengerProgram().run();
     }
     public void HengerProgram(){
-        List Henger = new Henger(4);
+        run();
     }
     public double atlagTerfogat(){
         double atlagTerfogat = 0;
@@ -20,11 +24,17 @@ public class HengerProgram {
         double csovekSulya = 0;
         return csovekSulya;
     }
-    private List Lista(){
-        return null;
+    private List<Henger> lista(){
+       hengerek = new ArrayList<>();
+       hengerek.add(new  Henger(1, 1));
+       hengerek.add(new TomorHenger(1, 2, 1));
+       hengerek.add(new Cso(1, 1, 1));
+       hengerek.add(new Cso(1, 1, 0.5));
+       return Collections.unmodifiableList(hengerek);
+       
     }
     public  void run(){
-        
+       lista();
     }
     
 }
